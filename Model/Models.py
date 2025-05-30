9e6f677f35e6c97550a2c9a7053bee3b072ea6cd
Zhao Chongyu <522653608@qq.com>
2025-05-30 21:35:29 +0800

    update model

--- Model/Models.py
+++ Model/Models.py
@@ -1,16 +1,24 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+# 从torch 引入 resnet18 并加载预训练参数
+from torchvision.models import resnet18
+from torchvision.models import ResNet18_Weights
 
 
 # 图像嵌入层，使用简单 CNN
 class ImageEmbedding(nn.Module):
-    def __init__(self, embed_dim, num_layers=5, dropout_rate=0.2):
+    def __init__(self, embed_dim, num_layers=5, dropout_rate=0.2, is_resnet=False):
         super(ImageEmbedding, self).__init__()
         self.cnn_layers = nn.ModuleList()
+        self.resnet = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
+        self.resnet.fc = nn.Linear(self.resnet.fc.in_features, embed_dim)
+        self.is_resnet = is_resnet
+
         in_channels = 3
         out_channels = 8
 
+        # 使用 num_layers 层的 CNN, no residuals
         for _ in range(num_layers):
             self.cnn_layers.append(nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1))
             self.cnn_layers.append(nn.BatchNorm2d(out_channels))
@@ -19,7 +27,6 @@ class ImageEmbedding(nn.Module):
             self.cnn_layers.append(nn.Dropout2d(dropout_rate))
             in_channels = out_channels
             out_channels *= 2
-
         # 计算经过 CNN 后图像的尺寸
         h, w = 256 // (2 ** num_layers), 256 // (2 ** num_layers)
         self.fc = nn.Linear(in_channels * h * w, embed_dim)
@@ -30,10 +37,13 @@ class ImageEmbedding(nn.Module):
         # 合并 batch、seq 和 camera 维度
         images = images.view(-1, 3, 256, 256)
         out = images
-        for layer in self.cnn_layers:
-            out = layer(out)
-        out = out.view(batch_size * seq_length * num_cameras, -1)
-        embedded = self.fc(out)
+        if not self.is_resnet:
+            for layer in self.cnn_layers:
+                out = layer(out)
+            out = out.view(batch_size * seq_length * num_cameras, -1)
+            embedded = self.fc(out)
+        else:
+            embedded = self.resnet(out)
         # 恢复 batch 和 seq 维度
         embedded = embedded.view(batch_size, seq_length, num_cameras, -1)
         # 对两个相机的特征进行拼接
@@ -47,7 +57,7 @@ class MotorEmbedding(nn.Module):
         super(MotorEmbedding, self).__init__()
         self.fc_layers = nn.ModuleList()
         in_dim = motor_dim
-        hidden_dim = 128
+        hidden_dim = 64
 
         for _ in range(num_fc_layers - 1):
             self.fc_layers.append(nn.Linear(in_dim, hidden_dim))
@@ -117,7 +127,7 @@ class MultimodalTransformer(nn.Module):
         self.transformer_decoder = TransformerDecoderModel(embed_dim, nhead, num_layers, 12)
         self.output_seq_length = output_seq_length
 
-    def forward(self, image_embedded, motor_embedded, tgt_embed, num_candidates=1, temperature=0.8):
+    def forward(self, image_embedded, motor_embedded, tgt_embed, num_candidates=5, temperature=0.8):
         # 拼接图像和电机信号的嵌入
         combined_embedded = torch.cat([motor_embedded, image_embedded], dim=-1)
 
@@ -151,14 +161,14 @@ class MultimodalTransformer(nn.Module):
 # 示例使用
 if __name__ == "__main__":
     import time
-    embed_dim = 128
+    embed_dim = 32
     nhead = 4
     num_layers = 1
     max_seq_length = 10
     output_seq_length = 5
 
-    image_embedding = ImageEmbedding(embed_dim)
-    motor_embedding = MotorEmbedding()
+    image_embedding = ImageEmbedding(embed_dim, is_resnet=False)
+    motor_embedding = MotorEmbedding(embed_dim=embed_dim)
     multimodal_transformer = MultimodalTransformer(embed_dim, nhead, num_layers, output_seq_length, max_seq_length)
 
     # 输入图像形状为 (batch_size, seq_length, 2, 3, 256, 256)
@@ -166,14 +176,13 @@ if __name__ == "__main__":
     # 输入电机数据形状为 (batch_size, seq_length, 12)
     motor_data = torch.randn(1, max_seq_length, 12)
 
-    # 生成 tgt_embed 示例输入
-    tgt_embed = torch.randn(1, output_seq_length, embed_dim * 3)
-
     start_time = time.time()
     for i in range(10):
         image_embedded = image_embedding(images)
         motor_embedded = motor_embedding(motor_data)
-        candidates = multimodal_transformer(image_embedded, motor_embedded, tgt_embed)
+
+        tgt_embed = torch.zeros((1, output_seq_length, embed_dim * 3)).to(motor_data.device)
+        candidates = multimodal_transformer(image_embedded, motor_embedded, tgt_embed, num_candidates=1, temperature=0.8)
         print(f"Generated {len(candidates)} candidates.")
     print(f"Time taken for 10 iterations: {time.time() - start_time:.2f} seconds")
 
@@ -203,3 +212,5 @@ if __name__ == "__main__":
     print(f"Transformer Encoder Size: {transformer_encoder_size:.2f} MB")
     print(f"Transformer Decoder Size: {transformer_decoder_size:.2f} MB")
     print(f"Total Model Size: {total_model_size:.2f} MB")
+
+
