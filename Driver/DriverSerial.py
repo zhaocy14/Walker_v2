268531bbedb3281e3be6710ba3819f8f0a8d4401
Zhao Chongyu <522653608@qq.com>
2025-11-14 14:56:23 +0800

    update DriverSerial.py

--- Driver/DriverSerial.py
+++ Driver/DriverSerial.py
@@ -1,3 +1,7 @@
+import os,sys
+pwd = os.path.abspath(os.path.abspath(__file__))
+father_path = os.path.abspath(os.path.dirname(pwd) + os.path.sep + "..")
+sys.path.append(father_path)
 import pymodbus
 from pymodbus.client import ModbusSerialClient
 from sympy.strategies.branch import condition
