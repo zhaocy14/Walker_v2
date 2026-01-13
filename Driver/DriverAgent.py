3f32b5a40d58e1d0fdf761e03c77fa7efa2e9a1c
Zhao Chongyu <522653608@qq.com>
2026-01-13 16:55:59 +0800

    update lidar

--- Driver/DriverAgent.py
+++ Driver/DriverAgent.py
@@ -87,11 +87,10 @@ class DriverAgent(object):
         set the wheel speed to the driver
         :return:
         """
-        left_rpm = int(self._left_spd / (2 * 3.14 * self.wheel_radius) * 60)
+        left_rpm = -int(self._left_spd / (2 * 3.14 * self.wheel_radius) * 60)
         self.driver_serial.set_single_driver_speed(rpm=left_rpm, motor='left')
         right_rpm = int(self._right_spd / (2 * 3.14 * self.wheel_radius) * 60)
         self.driver_serial.set_single_driver_speed(rpm=right_rpm, motor='right')
-        print(left_rpm, right_rpm)
 
     def enable_driver(self, enable: bool = True):
         """
