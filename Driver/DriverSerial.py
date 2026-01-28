370c49e245914011a82f2a531f50697e131a0fa8
Zhao Chongyu <522653608@qq.com>
2026-01-28 16:30:19 +0800

    update driver

--- Driver/DriverSerial.py
+++ Driver/DriverSerial.py
@@ -294,7 +294,7 @@ if __name__ == "__main__":
 
     # # #
     # driver.set_single_driver_speed(rpm=-30, motor='left')
-    driver.set_single_driver_speed(rpm=30, motor='right')
+    driver.set_single_driver_speed(rpm=-30, motor='right')
     time.sleep(1)
 
     driver.set_motor_enable(enable=False, motor='left')
