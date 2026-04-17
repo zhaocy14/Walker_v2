0769d52fa76721063e8e4b8ac443b6b270911e09
Zhao Chongyu <522653608@qq.com>
2026-04-17 17:44:43 +0800

    test LiDAR obstacle

--- Sensors/SensorConfig.py
+++ Sensors/SensorConfig.py
@@ -50,7 +50,8 @@ FILTER_THETA = 150
 # new version of filtering useless data
 # all numerical represent *.* m
 WALKER_TOP_BOUNDARY = int(0.1039 * SCAN_UNIT) # 0.10386m
-WALKER_BOTTOM_BOUNDARY = int(0.4231 * SCAN_UNIT * 2) # 0.42314m x2, considering the backward area
+# WALKER_BOTTOM_BOUNDARY = int(0.4231 * SCAN_UNIT * 2) # 0.42314m x2, considering the backward area
+WALKER_BOTTOM_BOUNDARY = int(0.5 * SCAN_UNIT * 2) # 0.42314m x2, considering the backward area
 WALKER_LEFT_BOUNDARY = int(0.354 * SCAN_UNIT)  # 0.354m
 WALKER_RIGHT_BOUNDARY = int(0.354 * SCAN_UNIT)  # 0.354m
 WALKER_BOX_BOUNDARY_VERTICAL = int(0.018 * SCAN_UNIT) # approximately the wheel diameter
